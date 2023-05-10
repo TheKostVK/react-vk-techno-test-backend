40c679958e6099d929bc87935badbe115bd67e9d
Vlad Kost <kostvkthekost@gmail.com>
2023-05-10 14:10:51 +0300

    rework register validation

--- validations/auth/Register.js
+++ validations/auth/Register.js
@@ -4,5 +4,4 @@ export const registerValidation = [
     body('email', 'Неверный формат почты').isEmail(),
     body('password', 'Пароль должен быть минимум 5-и символов').isLength({min: 5}),
     body('userName', 'Укажите имя').isLength({min: 3}),
-    body('avatarUrl', 'Неверная ссылка на аватарку').optional().isURL(),
 ];
